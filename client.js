3e1c52f8b92ae42543cdcbaf8d00428ccd509be9
Tai <Tai.Delisle@telus.com>
2022-11-23 09:10:51 -0700

    update with name being sent to server

--- client.js
+++ client.js
@@ -6,7 +6,10 @@ const connect = function() {
     host: "localhost",
     port: 50541
   });
-
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    conn.write("Name: TAI");
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("data", (data) => {
