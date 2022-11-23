0c193a85952551d8d051b64fe0b4e3a33f26fafa
Tai <Tai.Delisle@telus.com>
2022-11-23 09:26:34 -0700

    move input functions to new file

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    console.log(`bye bye!`);
-    process.exit();
-  }
-};
 
 setupInput();
