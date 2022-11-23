8d47e9737b21562bb567dc005c969ea568f13ce8
Tai <Tai.Delisle@telus.com>
2022-11-23 10:04:40 -0700

    refactor constants into separate file

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = "localhost";
+const PORT = 50541;
+const MOVE_UP_KEY = '\u0077'; //w
+const MOVE_LEFT_KEY = '\u0061'; //a
+const MOVE_DOWN_KEY = '\u0073'; //s
+const MOVE_RIGHT_KEY = '\u0064'; //d
+const CHAT_ONE_KEY = '\u006A'; //j
+const CHAT_TWO_KEY = '\u006B'; //k
+const CHAT_THREE_KEY = '\u006C'; //l
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  CHAT_ONE_KEY,
+  CHAT_TWO_KEY,
+  CHAT_THREE_KEY
+};
