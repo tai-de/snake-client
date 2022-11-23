59daad02b212c9dd452de990ac06f22a16b6d79f
Tai <Tai.Delisle@telus.com>
2022-11-23 09:26:42 -0700

    move input functions to new file

---
+++ input.js
@@ -0,0 +1,19 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log(`bye bye!`);
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
